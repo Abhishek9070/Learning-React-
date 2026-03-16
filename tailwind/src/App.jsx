0a77be04f962b14e8e643696a9a6692dc4e97467
Abhishek9070 <abhishekmishra051028@gmail.com>
2026-03-16 12:07:38 +0530

    feat: add React and Vite setup with Tailwind CSS integration
    - Created main application entry point with React and StrictMode.
    - Added Vite configuration for React and Tailwind CSS.
    - Introduced global styles in index.css with custom CSS variables for light and dark themes.
    - Added SVG assets for React and Vite logos.

---
+++ tailwind/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-black-500  bg-green-400">
+      Tailwind + Vite Working
+    </h1>
+    </>
+  )
+}
+
+export default App
