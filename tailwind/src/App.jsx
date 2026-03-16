5c8c32518966727eb4fe3c81aca4d81c0527421f
Abhishek9070 <abhishekmishra051028@gmail.com>
2026-03-16 22:53:46 +0530

    feat: implement Card component and update App layout

--- tailwind/src/App.jsx
+++ tailwind/src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <h1 className="text-4xl font-bold text-black-500  bg-green-400">
+      <h1 className="text-4xl font-bold  bg-amber-500 rounded-2xl  text-shadow-amber-800">
       Tailwind + Vite Working
+
     </h1>
     </>
   )
--- tailwind/src/main.jsx
+++ tailwind/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import Card from '../components/Card.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <br></br>
+    <Card name="Abhishek" button="Contact me" />
+    <Card name="Lucky" button="About me" />
   </StrictMode>,
 )
